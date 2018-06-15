74159700f2ee1202b46d0fe513b557f1e6ccf568
Kelsey Griffith <kelseyngriffith@gmail.com>
2018-06-15 01:55:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ function holidayCountdown(days,holiday) {
   return `It's ${days} days until ${holiday}!`
 }
 console.log(holidayCountdown(20,"Independence Day"))
+console.log(happyHolidays)
+console.log(happyHolidayTo("Halloween","Jessica"))
+console.log(happyHolidays("Allison"))
